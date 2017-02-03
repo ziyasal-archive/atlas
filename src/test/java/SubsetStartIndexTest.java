dcc88cfb02a53105521b3775e26a226e9f813401
Ziya SARIKAYA <ziya.sarikaya@here.com>
2017-02-03 15:52:40 +0100

    performed code formatting

--- src/test/java/SubsetStartIndexTest.java
+++ src/test/java/SubsetStartIndexTest.java
@@ -1,11 +1,12 @@
 import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class SubsetStartIndexTest  {
+public class SubsetStartIndexTest {
 
     /*
     Returns start index of subset if one contains another for given lists, otherwise returns -1 
@@ -23,10 +24,10 @@ public class SubsetStartIndexTest  {
     Expected: 1
      */
     @Test
-    public void find_Index_Test(){
-        ArrayList<Integer> list1 = new ArrayList<Integer>(Arrays.asList(1,4,5,4,5,3,2));
+    public void find_Index_Test() {
+        ArrayList<Integer> list1 = new ArrayList<Integer>(Arrays.asList(1, 4, 5, 4, 5, 3, 2));
 
-        ArrayList<Integer> list2 = new ArrayList<Integer>(Arrays.asList(3,2));
+        ArrayList<Integer> list2 = new ArrayList<Integer>(Arrays.asList(3, 2));
 
         int index = find(list1, list2);
 
@@ -35,23 +36,17 @@ public class SubsetStartIndexTest  {
 
     // ⚠ : Not optimized
     private static int find(List<Integer> list1, List<Integer> list2) {
-
         for (int i = 0; i <= list1.size() - list2.size(); i++) {
-
             if (list1.get(i).equals(list2.get(0))) {
 
                 Boolean found = true;
                 for (int j = 1; j < list2.size() && found; j++) {
-
                     if (!list1.get(i + j).equals(list2.get(j))) {
-
                         found = false;
                     }
                 }
 
-                if (found)
-                    return i;
-
+                if (found) return i;
             }
         }
 
